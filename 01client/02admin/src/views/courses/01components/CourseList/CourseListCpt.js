78ea22ea6d9008701a0f460467e00b678a2f14e4
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2019-10-29 14:52:27 +0800

    页面跳转

--- 01client/02admin/src/views/courses/01components/CourseList/CourseListCpt.js
+++ 01client/02admin/src/views/courses/01components/CourseList/CourseListCpt.js
@@ -4,17 +4,17 @@ import "./CourseListCpt.css";
 import net from "../../../../utils/net";
 import { Link } from "react-router-dom";
 
-
 export default class MyUserAdd extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      courses: [],
-    }
-  };
+      courses: []
+    };
+  }
   showProps() {
     console.log(this.props.match.path);
-  };
+  }
   deletUser(courseId) {
     let data = this.state.data;
     for (let i = 0; i < data.length; i++) {
@@ -25,99 +25,100 @@ export default class MyUserAdd extends React.Component {
     }
 
     this.setState({ data: data });
-  };
+  }
   componentDidMount() {
     let that = this;
-    net.get("courses/all",
-    {},
-    function (ob) {
+    net.get("courses/all", {}, function(ob) {
       that.setState({
         courses: ob.data.object
       });
-    }
-    )
-  };
-  handle() { 
-    const w = window.open('about:blink');
-    w.location.href="../../add/CoursesAdd.js"
-  };
+    });
+  }
+  toCoursesAdd() {
+    this.props.props.history.push(`/home/courses/add`);
+  }
   // 已发布个数
-  releasedNum(){ 
+  releasedNum() {
     var releasedNum = 0;
-    var courses = this.state.courses
+    var courses = this.state.courses;
     for (let i = 0; i < courses.length; i++) {
-      if (courses[i].state==2) {
+      if (courses[i].state == 2) {
         releasedNum++;
       }
     }
     return releasedNum;
-  };
+  }
   // 更新中个数
   updatingNum() {
     var updatingNum = 0;
-    var courses = this.state.courses
+    var courses = this.state.courses;
     for (let i = 0; i < courses.length; i++) {
       if (courses[i].state == 1) {
         updatingNum++;
       }
     }
     return updatingNum;
-  };
+  }
   // 未发布个数
   unreleasedNum() {
     var unreleasedNum = 0;
-    var courses = this.state.courses
+    var courses = this.state.courses;
     for (let i = 0; i < courses.length; i++) {
       if (courses[i].state == 0) {
         unreleasedNum++;
       }
     }
     return unreleasedNum;
-  };
+  }
   render() {
     const columns = [
       {
-        title: '课程编号',
-        dataIndex: 'id',
-        key: 'id'
+        title: "课程编号",
+        dataIndex: "id",
+        key: "id"
       },
       {
-        title: '课程状态',
-        dataIndex: 'state',
-        key: 'state',
+        title: "课程状态",
+        dataIndex: "state",
+        key: "state",
         render: text => {
-          if (text==0) {
-            return "未发布"
-          } else if (text==1) {
-            return "更新中"
-          } else if (text==2){
-            return "已发布"
+          if (text == 0) {
+            return "未发布";
+          } else if (text == 1) {
+            return "更新中";
+          } else if (text == 2) {
+            return "已发布";
           }
         }
       },
       {
-        title: '名称',
-        dataIndex: 'name',
-        key: 'name',
+        title: "名称",
+        dataIndex: "name",
+        key: "name"
       },
       {
-        title: '介绍',
-        dataIndex: 'introduce',
-        key: 'introduce',
+        title: "介绍",
+        dataIndex: "introduce",
+        key: "introduce"
       },
       {
-        title: '课程视频',
-        dataIndex: 'video',
-        key: 'video',
+        title: "课程视频",
+        dataIndex: "video",
+        key: "video"
       },
       {
-        title: '操作',
-        dataIndex: 'action',
-        key: 'action',
+        title: "操作",
+        dataIndex: "action",
+        key: "action",
         render: text => {
           return (
             <div>
-              <Button style={{ backgroundColor: "1px solid #CCCCCC" }} onClick={this.handle}>管理</Button>
+              <Button
+                style={{ backgroundColor: "1px solid #CCCCCC" }}
+                onClick={this.toCoursesAdd.bind(this)}
+              >
+                管理
+              </Button>
               <Divider type="vertical" />
               <Button>删除</Button>
             </div>
@@ -129,31 +130,35 @@ export default class MyUserAdd extends React.Component {
     return (
       <div>
         <div className="textMul">
-          <span>课程：
-            <strong className="mulNum">{this.state.courses.length}</strong>
-            个
+          <span>
+            课程：
+            <strong className="mulNum">{this.state.courses.length}</strong>个
           </span>
-          <span>已发布：
-            <strong className="mulNum">{this.releasedNum()}</strong>
-            个
+          <span>
+            已发布：
+            <strong className="mulNum">{this.releasedNum()}</strong>个
           </span>
-          <span>更新中：
-            <strong className="mulNum">{this.updatingNum()}</strong>
-            个
+          <span>
+            更新中：
+            <strong className="mulNum">{this.updatingNum()}</strong>个
           </span>
-          <span>未发布：
-            <strong className="mulNum">{this.unreleasedNum()}</strong>
-            个
+          <span>
+            未发布：
+            <strong className="mulNum">{this.unreleasedNum()}</strong>个
           </span>
         </div>
 
         <Table
           dataSource={this.state.courses}
           columns={columns}
-          style={{ width: "100%", margin: "0 auto", position: "relative" }}
-          pagination={{ pageSize: 12, position: "right" }} />
+          style={{
+            width: "100%",
+            margin: "0 auto",
+            position: "relative"
+          }}
+          pagination={{ pageSize: 12, position: "right" }}
+        />
       </div>
     );
   }
 }
-
--- 01client/02admin/src/views/courses/01components/courseTest/courseTest.js
+++ 01client/02admin/src/views/courses/01components/courseTest/courseTest.js
@@ -3,63 +3,59 @@ import "./courseTest.css";
 import { Icon, Button, Radio, Table, Upload, message } from "antd";
 import net from "../../../../utils/net";
 import CourseCreate from "../courseCreate/courseCreate";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const columns = [
   {
-    title: '名称',
-    dataIndex: 'name',
-    render: text => <a>{text}</a>,
+    title: "名称",
+    dataIndex: "name",
+    render: text => <a>{text}</a>
   },
   {
-    title: '状态',
-    dataIndex: 'age',
+    title: "状态",
+    dataIndex: "age"
   },
   {
-    title: '题目统计',
-    dataIndex: 'address',
+    title: "题目统计",
+    dataIndex: "address"
   },
   {
-    title: '时间限制',
-    dataIndex: 'address',
+    title: "时间限制",
+    dataIndex: "address"
   },
   {
-    title: '更新人/时间',
-    dataIndex: 'address',
+    title: "更新人/时间",
+    dataIndex: "address"
   },
   {
-    title: '操作',
-    dataIndex: 'address',
+    title: "操作",
+    dataIndex: "address"
   }
 ];
 const data = [
   {
-    key: '1',
-    name: 'John Brown',
+    key: "1",
+    name: "John Brown",
     age: 32,
-    address: 'New York No. 1 Lake Park',
+    address: "New York No. 1 Lake Park"
   }
 ];
 const props = {
-  name: 'file',
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
   headers: {
-    authorization: 'authorization-text',
+    authorization: "authorization-text"
   },
   onChange(info) {
-    if (info.file.status !== 'uploading') {
+    if (info.file.status !== "uploading") {
       console.log(info.file, info.fileList);
     }
-    if (info.file.status === 'done') {
+    if (info.file.status === "done") {
       message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
+    } else if (info.file.status === "error") {
       message.error(`${info.file.name} file upload failed.`);
     }
-  },
+  }
 };
 export default class CourseInformation extends React.Component {
   constructor(props) {
@@ -67,7 +63,7 @@ export default class CourseInformation extends React.Component {
     this.state = {
       flag: true
     };
-  };
+  }
   // componentDidMount() {
   //   console.log(this.props.match.path)
   // };
@@ -81,12 +77,16 @@ export default class CourseInformation extends React.Component {
   render() {
     const rowSelection = {
       onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        console.log(
+          `selectedRowKeys: ${selectedRowKeys}`,
+          "selectedRows: ",
+          selectedRows
+        );
       },
       getCheckboxProps: record => ({
-        disabled: record.name === 'Disabled User', // Column configuration not to be checked
-        name: record.name,
-      }),
+        disabled: record.name === "Disabled User", // Column configuration not to be checked
+        name: record.name
+      })
     };
     return (
       <div className="courseTestBox">
@@ -95,16 +95,27 @@ export default class CourseInformation extends React.Component {
           <div className="btnBox">
             <Button
               onClick={this.showImportBox.bind(this)}
-              style={{ backgroundColor: "#278BF5", color: 'white', width: 100, fontSize: "12px", marginRight: "5px" }}>
+              style={{
+                backgroundColor: "#278BF5",
+                color: "white",
+                width: 100,
+                fontSize: "12px",
+                marginRight: "5px"
+              }}
+            >
               <Icon type="plus" />
               <span>导入试卷</span>
             </Button>
-            <Link
-              to={`${this.props.match.path}/created`}
-              style={{ color: "white" }}>
+            <Link to={`/home/courses/add/created`} style={{ color: "white" }}>
               <Button
                 // onClick={this.onChange.bind(this)}
-                style={{ backgroundColor: "#278BF5", color: 'white', width: 100, fontSize: "12px" }}>
+                style={{
+                  backgroundColor: "#278BF5",
+                  color: "white",
+                  width: 100,
+                  fontSize: "12px"
+                }}
+              >
                 <Icon type="plus" />
                 <span>创建试卷</span>
               </Button>
@@ -117,7 +128,9 @@ export default class CourseInformation extends React.Component {
                 </div>
                 <div className="importUpload">
                   <Upload {...props} className="ant-upload">
-                    <Button style={{ backgroundColor: "#43BC60", color: "white" }}>
+                    <Button
+                      style={{ backgroundColor: "#43BC60", color: "white" }}
+                    >
                       <Icon type="upload" /> 上传文件
                     </Button>
                   </Upload>
@@ -127,8 +140,13 @@ export default class CourseInformation extends React.Component {
             </div>
           </div>
         </div>
-        <Table rowSelection={rowSelection} columns={columns} dataSource={data} style={{ width: "98.5%", margin: "0 auto", margin: "10px" }}
-          pagination={{ pageSize: 12, position: "right" }} />
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={data}
+          style={{ width: "98.5%", margin: "0 auto", margin: "10px" }}
+          pagination={{ pageSize: 12, position: "right" }}
+        />
         <Button style={{ backgroundColor: "#ECECEC" }}>删除</Button>
       </div>
     );
