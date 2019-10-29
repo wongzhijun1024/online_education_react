636f0390029502552327d68e0348d5e6b03f9aa3
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2019-10-29 16:21:36 +0800

    视频例子

--- 01client/02admin/src/views/examination/query/Query.js
+++ 01client/02admin/src/views/examination/query/Query.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Query.css";
-import { Button,Table,Tabs } from "antd";
+import { Player } from "video-react";
+import { Button, Table, Tabs } from "antd";
 export default class Query extends React.Component {
   constructor() {
     super();
-    this.state = {
-    };
-  };
+    this.state = {};
+  }
   render() {
     return (
       <div className="teacherQueryView">
+        <Player ref="player" videoId="video-1">
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </Player>
       </div>
     );
   }
