d2a305182ec19010fdb41b89efc47ca2d7f9a613
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2019-10-29 14:35:16 +0800

    课程路由跳转

--- 01client/02admin/src/views/courses/add/CoursesAdd.js
+++ 01client/02admin/src/views/courses/add/CoursesAdd.js
@@ -1,50 +1,59 @@
 import React from "react";
 import "./CoursesAdd.css";
-import {Tabs} from "antd";
+import { Tabs } from "antd";
 import net from "../../../utils/net";
 import CourseInformation from "../01components/courseInformation/CourseInformation.js";
 import CourseTest from "../01components/courseTest/courseTest";
 import CourseCreate from "../01components/courseCreate/courseCreate";
-import CourseDocument  from "../01components/courseDocument/CourseDocument.js"
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import CourseDocument from "../01components/courseDocument/CourseDocument.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const { TabPane } = Tabs;
 
 export default class CoursesAdd extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      activeKey: "1",
       fileList: [],
       value: 0
     };
   }
-   callback(key) {
-
+  onTabsChange = activeKey => {
+    this.setState({ activeKey });
+    switch (activeKey) {
+      case "3":
+        this.props.history.push(`${this.props.match.path}/list`);
+        break;
     }
-  onChange = e => {
-    console.log('radio checked', e.target.value);
-    this.setState({
-      value: e.target.value,
-    });
   };
-  
+
   render() {
     return (
-      <div className = "coursesAdd">
-        <Tabs className = "tabs" defaultActiveKey="1" onChange={this.callback.bind(this)}>
-          <TabPane className = "courseTabs" tab="课程文件" key="1">
-             <CourseDocument></CourseDocument>
+      <div className="coursesAdd">
+        <Tabs
+          className="tabs"
+          activeKey={this.state.activeKey}
+          defaultActiveKey="1"
+          onChange={this.onTabsChange.bind(this)}
+        >
+          <TabPane className="courseTabs" tab="课程文件" key="1">
+            <CourseDocument></CourseDocument>
           </TabPane>
           <TabPane tab="课程信息" key="2">
             <CourseInformation></CourseInformation>
           </TabPane>
           <TabPane tab="课程试卷" key="3">
             <Switch>
-              <Route exact path={`${this.props.match.path}`} component={CourseTest}></Route>
-              <Route exact path={`${this.props.match.path}/created`} component={CourseCreate}></Route>
+              <Route
+                exact
+                path={`${this.props.match.path}/list`}
+                component={CourseTest}
+              ></Route>
+              <Route
+                exact
+                path={`${this.props.match.path}/created`}
+                component={CourseCreate}
+              ></Route>
             </Switch>
           </TabPane>
           <TabPane tab="课程题库" key="4">
