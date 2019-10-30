7fe8b1d5cfc8bd7b9785902230f56bbed7c57395
DSH47 <1461301563@qq.com>
2019-10-30 13:38:26 +0800

    addBank

--- 01client/02admin/src/components/Content/Content.js
+++ 01client/02admin/src/components/Content/Content.js
@@ -8,6 +8,7 @@ import TeacherQuery from "../../views/teacher/query/Query";
 import TeacherAdd from "../../views/teacher/add/Add";
 import ExaminationQuery from "../../views/examination/query/Query";
 import ExaminationAdd from "../../views/examination/add/Add";
+import ExaminationBank from "../../views/examination/bank/bank.js"
 import CoursesList from "../../views/courses/list/CoursesList";
 import CoursesAdd from "../../views/courses/add/CoursesAdd";
 import Test from "../../views/test/Test";
@@ -32,6 +33,7 @@ export default class MyContent extends React.Component {
           <Route exact  path={"/home/teacher/add"} component={TeacherAdd} />
           <Route exact path={"/home/user/query"} component={UserQuery} />
           <Route exact path={"/home/user/infor"} component={User} />
+          <Route exact path={"/home/examination/bank"} component={ExaminationBank} />
         </Switch>
       </Content>
     );
--- 01client/02admin/src/views/examination/add/Add.js
+++ 01client/02admin/src/views/examination/add/Add.js
@@ -1,266 +1,191 @@
 import React from "react";
 import "./Add.css";
-import { Table,Upload, message,Icon, Button,Cascader } from "antd";
 import net from "../../../utils/net";
+import { Button, Cascader, Table,Radio } from "antd";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-// 添加数据
+
 const options = [
   {
-    value: 'C++',
-    label: 'C++',
+    value: "C++",
+    label: "C++",
     children: [
       {
-        value: 'hangzhou',
-        label: 'Hangzhou',
+        value: "hangzhou",
+        label: "Hangzhou",
         children: [
           {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
+            value: "xihu",
+            label: "West Lake"
+          }
+        ]
+      }
+    ]
   },
   {
-    value: '数据结构',
-    label: '数据结构',
+    value: "数据结构",
+    label: "数据结构",
     children: [
       {
-        value: 'nanjing',
-        label: 'Nanjing',
+        value: "nanjing",
+        label: "Nanjing",
         children: [
           {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
+            value: "zhonghuamen",
+            label: "Zhong Hua Men"
+          }
+        ]
+      }
+    ]
   },
   {
-    value: 'Python',
-    label: 'Python',
+    value: "Python",
+    label: "Python",
     children: [
       {
-        value: 'nanjing',
-        label: 'Nanjing',
+        value: "nanjing",
+        label: "Nanjing",
         children: [
           {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
+            value: "zhonghuamen",
+            label: "Zhong Hua Men"
+          }
+        ]
+      }
+    ]
   },
   {
-    value: 'Java',
-    label: 'Java',
+    value: "Java",
+    label: "Java",
     children: [
       {
-        value: 'nanjing',
-        label: 'Nanjing',
+        value: "nanjing",
+        label: "Nanjing",
         children: [
           {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
+            value: "zhonghuamen",
+            label: "Zhong Hua Men"
+          }
+        ]
+      }
+    ]
+  }
 ];
-const props = {
-  name: 'file',
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  headers: {
-    authorization: 'authorization-text',
+const columns = [
+  {
+    title: '题干',
+    dataIndex: 'name',
+    render: text => <a>{text}</a>,
   },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
+  {
+    title: '类型',
+    dataIndex: 'age',
   },
-};
+  {
+    title: '最后更新',
+    dataIndex: 'address',
+  },
+  {
+    title: '操作',
+    dataIndex: 'action',
+  },
+
+];
+const data = [];
 function onChange(value) {
   console.log(value);
+}
+
+// Just show the latest item.
+function displayRender(label) {
+  return label[label.length - 1];
+}
+
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === "Disabled User", // Column configuration not to be checked
+    name: record.name
+  })
 };
 
-const data = [];
-for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`,
-  });
-}
-export default class Add extends React.Component {
-  constructor() {
-    super();
+export default class ExaminationAdd extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
     this.state = {
-      fileList: [],
-      size: 'large',
-      selectedRowKeys: [],
-      loading: false
+      data: [],
     };
+
+  }
+  tobank(){
+    this.props.history.push(`/home/examination/bank`);
   }
 
-  newData() {
-    for (let i = 0; i < 46; i++) {
-      this.state.data.push({
-        key: i,
-        type: `Edward King ${i}`,
-        chapter: 'bdehbchde',
-      });
-    }
-  }
-  start = () => {
-    this.setState({
-      loading: true
-    });
-    // ajax request after empty completing
-    setTimeout(() => {
-      this.setState({
-        selectedRowKeys: [],
-        loading: false,
-      });
-    }, 1000);
-  };
-
-
-  deletUser(id) {
-    let length = this.state.data.length;
-    let data = this.state.data;
-    for (let i = 0; i < length; i++) {
-      if (data[i].id == id) {
-        data.splice(i, 1);
-        data[i]=data[i-1];  
-        break;
-      }
-    }
-    this.setState({
-      data: data
-    });
-  };
-
-
-  onSelectChange = selectedRowKeys => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-    this.setState({
-      selectedRowKeys
-    });
-  };
-
-  
-  handleSizeChange = e => {
-    this.setState({ size: e.target.value });
-  };
-
-  showAdd() { 
-    this.refs.addBox.style.display = 'block';
-  };
-  closeAdd() {
-    this.refs.addBox.style.display = 'none';
-  };
-
-  
   render() {
-    const { size } = this.state;
-    const columns = [
-            {
-                title: '课程ID',
-                dataIndex: 'id',
-                key: 'id',
-                render: text => <a>{text}</a>,
-            },
-            {
-                title: '科目',
-                dataIndex: 'type',
-                key: 'types',
-                render: text => <a>{text}</a>,
-            },
-            {
-                title: '章节',
-                dataIndex: 'chapter',
-                key: 'chapter',
-            },
-            {
-                title: '题库文件',
-                dataIndex: 'file',
-                key: 'file',
-            },
-            {
-                title: '是否删除',
-                key: 'action',
-                render: (text, record) => (
-                    <span>
-                        {/* <a>Invite {record.name}</a> */}
-                        {/* <Divider type="vertical" /> */}
-                        {/* //传入text的id作为实参 */}
-                        <a onClick={this.deletUser.bind(this, text.id)}>删除</a>
-                    </span>
-                ),
-            },
-    ];
-    
-    const {
-      loading,
-      selectedRowKeys
-    } = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
-    };
-    const hasSelected = selectedRowKeys.length > 0;
-    
     return (
-      <div className="addView">
-        <div className="addExam">
-          <span className="addTitle">题库添加</span>
+      <div className="coursesBankBox">
+        {/* 头部 */}
+        <div className="coursesBankHead">
+          <div className="coursesBankTitle">题库添加</div>
+          {/* <Link
+          to="/home/examination/bank"> */}
+          <Button
+            type="primary"
+            style={{ background: "#278BF5" }}
+            onClick={this.tobank.bind(this)}
+          >
+            +单选题
+          </Button>
+          {/* </Link> */}
+         
         </div>
-        <div className="btnBox">
-          <Button icon="plus" className="addBtn" size={size} style={{ backgroundColor: '#32CCA8', color: 'white' }} onClick={this.showAdd.bind(this)}>
-          添加
-        </Button>
-        <Button icon="delete" className="deleteBtn" size={size} style={{backgroundColor:'#E6BC1A',color:'white'}}>
-          删除
-        </Button>
-        </div>
-        <div className="examBox">
-          <div className="listContent"><Icon type="unordered-list" />内容列表</div>
-          <div className="addBox" ref='addBox'>
-           <div className="add">
-             <div className="addType">
-                 <span>科目类型：</span>
-                 <Cascader
-                   defaultValue={['zhejiang', 'hangzhou', 'xihu']}
-                   options={options}
-                   onChange={onChange}
-                   className='typeBtn'
-                 />
-             </div>
-            <Upload {...props} className="upBtn">
-                    <Button>
-                    <Icon type="upload" /> 上传试卷
-                    </Button>
-            </Upload>
-              <Button className="savaBtn" ref="savaBtn" onClick={this.closeAdd.bind(this)}>保存</Button>
-            </div>
+        {/* 选择器 */}
+        <div className="BankSeletBox">
+          <div className="left-Select">
+            <Cascader
+              options={options}
+              expandTrigger="hover"
+              displayRender={displayRender}
+              onChange={onChange}
+            />
           </div>
-           <div>
-        <div>
-          <span>
-            {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-          </span>
+          <Button
+            value="small"
+            type="primary"
+            style={{ background: "#43BB60", margin: "0px 8px 0px 0px" }}
+          >
+            搜索
+          </Button>
+          <Button
+            value="small"
+            type="primary"
+            style={{ background: "#43BB60" }}
+          >
+            导出题目
+          </Button>
         </div>
-        <Table rowSelection={rowSelection} columns={columns} dataSource={this.state.data} />
-      </div>
+        {/* 题库表单 */}
+        <div className="table-Bank">
+          <Table
+            rowSelection={rowSelection}
+            columns={columns}
+            dataSource={data}
+          />
+          {/* 删除按键 */}
+          
+          <div className="bank-button">
+          <Radio className="bk-butten">全选</Radio>
+          <Button type="primary" style={{ background: "#ECECEC",color:"black" }}>删除</Button>
+          </div>
+         
         </div>
       </div>
     );
