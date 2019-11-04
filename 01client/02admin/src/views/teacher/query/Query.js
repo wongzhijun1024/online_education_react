7df93834f2c79a3240d2fceefaa4d9098300969e
wwyy <1114789207@qq.com>
2019-11-04 18:23:08 +0800

    老师列表

--- 01client/02admin/src/views/teacher/query/Query.js
+++ 01client/02admin/src/views/teacher/query/Query.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Query.css";
-import { List, Avatar} from "antd";
+import { List, Avatar, Input, Divider, Drawer, Col, Table, Tabs,Button } from "antd";
 import net from "../../../utils/net";
+const { TabPane } = Tabs;
 export default class Query extends React.Component {
   constructor() {
     super();
@@ -10,6 +11,21 @@ export default class Query extends React.Component {
       data:[],
     };
   };
+
+
+  searchTeacher = value => {
+    //获得姓名
+    let name = value;
+    net.uploadFile(
+      "teacherQuery",
+      { name: name },
+      function (ob) {
+        if (ob != null) {
+
+        }
+      }
+    );
+  }
   componentDidMount() {
     let that = this;
     net.get("teachers", {}, function(ob) {
@@ -19,6 +35,7 @@ export default class Query extends React.Component {
     });
   }
   render() {
+
     return (
       <div className="box">
         <div className="title1">师资列表</div>
@@ -45,7 +62,51 @@ export default class Query extends React.Component {
     )}
   />
 </div>
-</div>
+        {/* <Tabs defaultActiveKey="1" onChange={callback} type="card" className="tab">
+          <TabPane tab="老师列表" key="1">
+          
+            <Table columns={columns} dataSource={this.state.teacherdata} className="tab1"/>
+            
+          </TabPane> */}
+          {/* <TabPane tab="老师介绍" key="2">
+            <div className="line">
+              <div className="line_left"></div>
+              <div className="mid">INTRODUCE</div>
+              <div className="line_right"></div>
+            </div>
+            <div>
+              {data.map(item => (
+
+                <div className="f">
+
+                  <img src={item.src} className="img"></img>
+                  <div className="item1">
+                    <div>
+                      <span className="span1">姓名：</span>
+                      <span className="span2">{item.name}</span>
+                    </div>
+                    <div>
+                      <span className="span1">爱好：</span>
+                      <span className="span2">222222222</span>
+                    </div>
+                    <div>
+                      <span className="span1">荣誉：</span>
+                      <span className="span2">222222222</span>
+                    </div>
+                  </div>
+                  <div className="item2">
+                    <div className="introduce">简介</div>
+                    <div className="content">{item.introduce}</div>
+                  </div>
+
+                </div>
+
+
+              ))}
+            </div>
+          </TabPane> */}
+        {/* </Tabs> */}
+      </div>
     );
   }
 }
